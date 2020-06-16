45779ebe80469bdb9a229018c7ab7220da565734
Nasreenkhan-Winjit <NasreenK@winjit.com>
2020-06-16 19:15:34 +0530

    commentedOnPOM1 commit

--- src/main/java/browsers/browserType/ChromeBrowser.java
+++ src/main/java/browsers/browserType/ChromeBrowser.java
@@ -1,6 +1,8 @@
 package browsers.browserType;
 
 import browsers.DriverManagerWeb;
+//import io.github.bonigarcia.wdm.WebDriverManager;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.PageLoadStrategy;
 import org.openqa.selenium.WebDriver;
